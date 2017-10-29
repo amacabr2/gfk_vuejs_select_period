e269ed914baad7864fe8806507b9e3e40317310b
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-10-29 14:41:25 +0100

    Calendar display in compliance with the display standards

--- src/components/CalendarPicker.vue
+++ src/components/CalendarPicker.vue
@@ -1,7 +1,17 @@
 <template>
     <div class="rangepicker">
         <div class="rangepicker_month" v-for="month in months">
-            {{ month.getName() }}
+            <div class="rangepicker_monthtitle">
+                {{ month.getName() }}
+            </div>
+            <div class="rangepicker_days">
+                <div v-for="day in days">{{ day }}</div>
+            </div>
+            <div class="rangepicker_numbers">
+                <div class="rangepicker_day" v-for="day in month.getDays()" :class="classForDay(day, month)">
+                    {{ day.getDate() }}
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -12,7 +22,8 @@
     export default {
         data() {
             return {
-                months: []
+                months: [],
+                days: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim']
             }
         },
 
@@ -22,6 +33,16 @@
 
         props: {
             year: Number
+        },
+
+        methods: {
+            classForDay(day, month) {
+                let classes = []
+                if (!month.contains(day)) {
+                    classes.push('rangepicker_out')
+                }
+                return classes
+            }
         }
     }
 </script>
